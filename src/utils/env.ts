47d3fb202edbcb5e16815e2333c9e7fdd9ec6513
Pedro Henrique <pedrohesm@gmail.com>
2025-04-02 20:37:53 -0300

    hotfix: configure for production

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,16 +1,11 @@
 import z from 'zod';
 
 const envSchema = z.object({
-  DEV_MODE: z.string().url(),
+  VITE_DEV_MODE: z.string().url(),
   VITE_API_URL_DEVELOPMENT: z.string(),
   VITE_API_URL_PRODUCTION: z.string(),
 });
 
-// Parse the environment variables
-const env = envSchema.safeParse(import.meta.env);
-if (!env.success) {
-  console.error('Invalid environment variables:', env.error.format());
-}
-
-// export const apiUrlDevelopment: string = env.data.VITE_API_URL_DEVELOPMENT;
-// export const apiUrlProduction: string = env.data.VITE_API_URL_PRODUCTION;
+export const apiUrlDevelopment = import.meta.env.VITE_API_URL_DEVELOPMENT;
+export const apiUrlProduction = import.meta.env.VITE_API_URL_PRODUCTION;
+export const isDev = import.meta.env.VITE_DEV_MODE === true;
